69254f3df7c84479e9912df375a4e767c03f9379
Luan Nico <luanpotter27@gmail.com>
2019-03-06 23:59:20 -0300

    better tests

--- src/test/java/com/packt/rest/todos/TodosApiApplicationTests.java
+++ src/test/java/com/packt/rest/todos/TodosApiApplicationTests.java
@@ -1,16 +1,28 @@
 package com.packt.rest.todos;
 
+import com.packt.rest.todos.todo.Todo;
+import com.packt.rest.todos.todo.TodoApi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TodosApiApplicationTests {
 
+	@Autowired
+	private TodoApi todoApi;
+
 	@Test
-	public void contextLoads() {
+	public void testListBothInitialTodos() {
+		List<Todo> todos = todoApi.listTodos();
+		assertThat(todos.size()).isEqualTo(2);
 	}
 
 }
